1093f8a1945aa0d0ac87881c4826e348481f1605
Mahdi Maheri Esfahani <maahdima@gmail.com>
2025-07-28 19:19:32 +0330

    feat(sidebar): enhance sidebar with conditional rendering and add footer with credits

--- ui/src/components/layout/app-sidebar.tsx
+++ ui/src/components/layout/app-sidebar.tsx
@@ -1,28 +1,57 @@
-import { IconRoute } from '@tabler/icons-react'
+import { IconHeartFilled, IconRoute } from '@tabler/icons-react'
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuItem,
   SidebarRail,
+  useSidebar,
 } from '@/components/ui/sidebar'
 import { NavGroup } from '@/components/layout/nav-group'
 import { sidebarData } from './data/sidebar-data'
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const { open } = useSidebar()
+
   return (
     <Sidebar collapsible='icon' variant='floating' {...props}>
-      <SidebarHeader className='flex-row items-center justify-center pt-5'>
-        <IconRoute />
-        <h1 className='text-2xl font-bold tracking-tight'>MWPanel</h1>
-      </SidebarHeader>
+      {open && (
+        <SidebarHeader className='flex-row items-center justify-center pt-5'>
+          <IconRoute />
+          <h1 className='text-2xl font-bold tracking-tight'>MWPanel</h1>
+        </SidebarHeader>
+      )}
+
       <SidebarContent>
         {sidebarData.navGroups.map((props) => (
           <NavGroup key={props.title} {...props} />
         ))}
       </SidebarContent>
-      {/*<SidebarFooter>*/}
-      {/*  <NavUser user={sidebarData.user} />*/}
-      {/*</SidebarFooter>*/}
+
+      {open && (
+        <SidebarFooter className='flex items-center justify-center p-4 text-white/80 shadow-inner'>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <div className='flex items-center justify-center gap-1 text-sm text-xs font-semibold'>
+                <span>Made with</span>
+                <IconHeartFilled size='15' />
+                <span>by</span>
+                <a
+                  href='https://github.com/Maahdima'
+                  className='text-xs'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Maahdima
+                </a>
+              </div>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarFooter>
+      )}
+
       <SidebarRail />
     </Sidebar>
   )
