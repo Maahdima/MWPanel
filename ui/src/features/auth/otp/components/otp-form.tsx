d29da5d8445e953fbf5e62eb9eedb4d428d489dd
Mahdi Maheri Esfahani <maahdima@gmail.com>
2026-09-11 11:10:54 +0330

    fix(otp): adjust OTP input layout by reordering input slots for improved user experience

--- ui/src/features/auth/otp/components/otp-form.tsx
+++ ui/src/features/auth/otp/components/otp-form.tsx
@@ -146,14 +146,11 @@ export function OtpForm({ className, ...props }: OtpFormProps) {
                   <InputOTPGroup>
                     <InputOTPSlot index={0} />
                     <InputOTPSlot index={1} />
-                  </InputOTPGroup>
-                  <InputOTPSeparator />
-                  <InputOTPGroup>
                     <InputOTPSlot index={2} />
-                    <InputOTPSlot index={3} />
                   </InputOTPGroup>
                   <InputOTPSeparator />
                   <InputOTPGroup>
+                    <InputOTPSlot index={3} />
                     <InputOTPSlot index={4} />
                     <InputOTPSlot index={5} />
                   </InputOTPGroup>
